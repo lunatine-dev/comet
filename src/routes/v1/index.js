d5322ecb237beb99090367cfbf32367f3340fefc
Lunatine <github@astatine.moe>
2025-06-02 02:43:04 +0100

    comment

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,6 @@
 export default async function (fastify) {
     fastify.get("/ping", (request, reply) => {
+        //just a test for server readiness
         return {
             message: "Pong!",
             timestamp: Date.now(),
